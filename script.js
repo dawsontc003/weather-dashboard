2a5fb8f8ad8814070c4894a8755b65a699629064
Thomas Dawson <dawsontc003@gmail.com>
2020-11-01 14:26:52 -0700

    Finished application with all required weather data and dynamic UV index color coding

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // variable for to display current date
 var date = moment().format("l");
+
 $(document).ready(function () {
   // API URL stored as variable...
   var apiURL =
@@ -32,6 +33,8 @@ $(document).ready(function () {
         fetch(allWeatherDataURL + lat + lon + allWeatherDataAPIkey)
           .then((response) => response.json())
           .then(function (weatherData) {
+            // store variable for UV index color coding
+            uviIndex = weatherData.current.uvi;
             // Appending new text value to "current-temp" html ID
             currentTemp.textContent =
               "Tempature:" + "  " + weatherData.current.temp + " " + "F";
@@ -49,47 +52,61 @@ $(document).ready(function () {
               "UV Index:" + "  " + weatherData.current.uvi;
             currentUVI.append();
             console.log(weatherData);
-            // Appending API value for day1 forcast tempature
-            day1Forcast.textContent =
+            // Appending API value for day1 forecast tempature
+            day1forecast.textContent =
               "Temp:" + "  " + weatherData.daily[0].temp.max;
-            day1Forcast.append();
-            // Appending API value for day2 forcast tempature
-            day2Forcast.textContent =
+            day1forecast.append();
+            // Appending API value for day2 forecast tempature
+            day2forecast.textContent =
               "Temp:" + "  " + weatherData.daily[1].temp.max;
-            day2Forcast.append();
-            // Appending API value for day3 forcast tempature
-            day3Forcast.textContent =
+            day2forecast.append();
+            // Appending API value for day3 forecast tempature
+            day3forecast.textContent =
               "Temp:" + "  " + weatherData.daily[2].temp.max;
-            day3Forcast.append();
-            // Appending API value for day4 forcast tempature
-            day4Forcast.textContent =
+            day3forecast.append();
+            // Appending API value for day4 forecast tempature
+            day4forecast.textContent =
               "Temp:" + "  " + weatherData.daily[3].temp.max;
-            day4Forcast.append();
-            // Appending API value for day5 forcast tempature
-            day5Forcast.textContent =
+            day4forecast.append();
+            // Appending API value for day5 forecast tempature
+            day5forecast.textContent =
               "Temp:" + "  " + weatherData.daily[4].temp.max;
-            day5Forcast.append();
+            day5forecast.append();
 
-            // Appending API value for day1 forcast humidity
+            // Appending API value for day1 forecast humidity
             day1Hum.textContent =
               "Humidity:" + " " + weatherData.daily[0].humidity + "%";
             day1Hum.append();
-            // Appending API value for day2 forcast humidity
+            // Appending API value for day2 forecast humidity
             day2Hum.textContent =
               "Humidity:" + " " + weatherData.daily[1].humidity + "%";
             day2Hum.append();
-            // Appending API value for day3 forcast humidity
+            // Appending API value for day3 forecast humidity
             day3Hum.textContent =
               "Humidity:" + " " + weatherData.daily[2].humidity + "%";
             day3Hum.append();
-            // Appending API value for day4 forcast humidity
+            // Appending API value for day4 forecast humidity
             day4Hum.textContent =
               "Humidity:" + " " + weatherData.daily[3].humidity + "%";
             day4Hum.append();
-            // Appending API value for day5 forcast humidity
+            // Appending API value for day5 forecast humidity
             day5Hum.textContent =
               "Humidity:" + " " + weatherData.daily[4].humidity + "%";
             day5Hum.append();
+            // condition to display color code for UV index
+            if (uviIndex > 7) {
+              var color = document.getElementById("current-uv");
+              color.classList.add("past");
+            } else if (uviIndex >= 3 || uviIndex <= 5) {
+              var color = document.getElementById("current-uv");
+              color.classList.add("present");
+            }
+            if (uviIndex < 3) {
+              var color = document.getElementById("current-uv");
+              color.classList.add("future");
+            }
+
+            console.log(weatherData.current.uvi);
           });
       });
   });
@@ -103,38 +120,40 @@ $(document).ready(function () {
   var currentWindSpeed = document.querySelector("#current-wind");
   // declared variable to target HTML for current UV Index
   var currentUVI = document.querySelector("#current-uv");
-  // declared variable to target HTML for day 1 of 5 day forcast
-  var day1Forcast = document.querySelector("#nextDay1");
-  // declared variable to target HTML for day 2 of 5 day forcast
-  var day2Forcast = document.querySelector("#nextDay2");
-  // declared variable to target HTML for day 3 of 5 day forcast
-  var day3Forcast = document.querySelector("#nextDay3");
-  // declared variable to target HTML for day 4 of 5 day forcast
-  var day4Forcast = document.querySelector("#nextDay4");
-  // declared variable to target HTML for day 5 of 5 day forcast
-  var day5Forcast = document.querySelector("#nextDay5");
-  // declared variable to target HTML for day 1 of 5 day forcast humidity
+  // declared variable to target HTML for day 1 of 5 day forecast
+  var day1forecast = document.querySelector("#nextDay1");
+  // declared variable to target HTML for day 2 of 5 day forecast
+  var day2forecast = document.querySelector("#nextDay2");
+  // declared variable to target HTML for day 3 of 5 day forecast
+  var day3forecast = document.querySelector("#nextDay3");
+  // declared variable to target HTML for day 4 of 5 day forecast
+  var day4forecast = document.querySelector("#nextDay4");
+  // declared variable to target HTML for day 5 of 5 day forecast
+  var day5forecast = document.querySelector("#nextDay5");
+  // declared variable to target HTML for day 1 of 5 day forecast humidity
   var day1Hum = document.querySelector("#humDay1");
-  // declared variable to target HTML for day 2 of 5 day forcast humidity
+  // declared variable to target HTML for day 2 of 5 day forecast humidity
   var day2Hum = document.querySelector("#humDay2");
-  // declared variable to target HTML for day 3 of 5 day forcast humidity
+  // declared variable to target HTML for day 3 of 5 day forecast humidity
   var day3Hum = document.querySelector("#humDay3");
-  // declared variable to target HTML for day 4 of 5 day forcast humidity
+  // declared variable to target HTML for day 4 of 5 day forecast humidity
   var day4Hum = document.querySelector("#humDay4");
-  // declared variable to target HTML for day 5 of 5 day forcast humidity
+  // declared variable to target HTML for day 5 of 5 day forecast humidity
   var day5Hum = document.querySelector("#humDay5");
+  // Title for 5 day forecast section
+  var searchSave = document.querySelector("#savedCity1");
   // Get weather details function declared in global memory
   function getWeatherData(lat, lon) {
     // Appending new text value to "city-and-date" html ID
-    cityDate.textContent = document.getElementById("city").value + " " + date;
+    cityDate.textContent =
+      document.getElementById("city").value + " " + "(" + date + ")";
     cityDate.append();
   }
-
+  // Setting local storage for search city history
   function storeCityInfo(apiURL, citySearch, apiKey) {
     localStorage.setItem("City1Search:", apiURL + citySearch + apiKey);
     localStorage.setItem("City1:", citySearch);
   }
-  // display city from previous search
 
   // Global scope function for callAPI function
   async function callAPI(apiURL, citySearch, apiKey) {
@@ -142,38 +161,3 @@ $(document).ready(function () {
     return response.json();
   }
 });
-// current day info City and date, temp. Huminity, wind speed, UV index
-
-// DONE // Set up UI with specified classes and ID's to target in JS
-// variable for current date (use moment js)
-// Declare variables in global memory for the API call URL value
-/////////// var for initial part of URL
-/////////// var for user input of city
-/////////// var for required key for API
-// Use variables for current day container for
-//////// user city searched
-/////// current date
-// target API data and store in variable
-////// tempature
-////// humidity
-////// wind speed
-////// UV index
-// use current day variable details to show API data in UI
-////// target HTML elements by ID and append new API data
-
-// 5 day forcast
-// Create card style UI with HTML (note: variables for day 1 are present from current day display)
-////// Var for date
-////// Var for temp day 2
-////// Var for temp day 3
-////// Var for temp day 4
-////// Var for temp day 5
-////// Var for Humidity day 2
-////// Var for Humidity day 3
-////// Var for Humidity day 4
-////// Var for Humidity day 5
-
-// persist history of last 5 searches
-////// set key value pairs to story data in local memory
-////// set local storage items
-//////set functionality to retrieve local storage items
